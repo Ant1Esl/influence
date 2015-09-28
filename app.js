4c657439401dd4aec1839216cb9ae714a039b88c
Antoine EISELE <Eisele.antoine@gmail.com>
2015-09-28 18:04:18 +0200

    Deuxième commit (websocket io)

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var io = require('socket.io'); //useless at the moment
 var app = require('express')();
+var http = require('http').Server(app);
 
-// var server = http.createServer(function (req, res){
-// res.end('you asked for something else');
-//});
+// Je met socket.io dans le serveur http
+var io = require('socket.io')(http);
 
-
-var http = require('http').Server(app);;
-
-app.get('/',function(req, res){
-//
+app.get('/',function(req,res){
+   // __dirname supergolobale
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-console.log('listening on *3000');
+http.listen(3000,function(){
+    console.log('listening on *3000');
+});
+
+io.on('connection',function(socket){
+   console.log('a user is connected');
 });
