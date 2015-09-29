1a1c6acbb0f9614b9d22a7a9251646aaf01d4ad8
Antoine EISELE <Eisele.antoine@gmail.com>
2015-09-29 15:31:59 +0200

    Système de vote avec mouse over

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-var app = require('express')();
-var http = require('http').Server(app);
+var express = require('express');
+var app = express();
 
-// Je met socket.io dans le serveur http
+var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/',function(req,res){
-   // __dirname supergolobale
-    res.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000,function(){
-    console.log('listening on *3000');
+var votes = [
+    'justin bieber':0,
+    'one direction':0];
+
+io.on('connection', function(socket){
+  socket.emit('message', 'welcome');
+  console.log('a user connected');
+  socket.on('choice', function(what){
+    console.log('chosen: ' + what);
+      if (what ==='justin bieber'){
+      votes['justin bieber']++;
+      }else{
+      votes['one direction']++;
+      }
+    console.log(votes);            
+  });
 });
 
-io.on('connection',function(socket){
-    var choices ='';
-    console.log('a user is connected');
-        socket.on('choice',function(what){
-            choices += ' '+what;
-            console.log('chosen: ' +what);
-            socket.emit('choices', choices);
-    });
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
