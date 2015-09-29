b31ec9793ead1c8e791889215d0341f093244b24
Antoine EISELE <Eisele.antoine@gmail.com>
2015-09-29 18:43:48 +0200

    Projet fini sans layer

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var choices = require('./choices.json');
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -8,22 +9,49 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-var votes = {
-    'justin bieber' :0,
-    'one direction' :0
+
+var votes = null;
+var random = 0;
+
+var start = function () {
+  random = Math.floor(Math.random()*choices.length);
+
+  votes =  choices[random];
+  votes.left.total = 0;
+  votes.right.total = 0;
+
+  io.sockets.emit('votes', votes);
 };
 
+setInterval(function(){
+  start();
+},3000);
+start();
+
 io.on('connection', function(socket){
-  socket.emit('message', 'welcome');
-  console.log('a user connected');
-  socket.on('choice', function(what){
-      if (what ==='justin bieber'){
-      votes['justin bieber']++;
-      }else{
-      votes['one direction']++;
-      }
-      socket.emit('total', votes);
+  // send all data
+  socket.emit('votes', votes);
+
+  // On hover
+  socket.on('onhover', function(what){
+    if (what === 'left') {
+      votes.left.total++;
+    } else {
+      votes.right.total++;
+    }
+    io.sockets.emit('total', votes);
   });
+
+  // Off hover
+  socket.on('offhover',function(what){
+    if (what === 'left') {
+      votes.left.total--;
+    } else {
+      votes.right.total--;
+    }
+    io.sockets.emit('total', votes);
+  })
+
 });
 
 http.listen(3000, function(){
